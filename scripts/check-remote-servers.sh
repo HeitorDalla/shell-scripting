561f3aa5b008bd850c90b4bd5ec6e482a7becd38
HeitorDalla <heitorvillavilla@gmail.com>
2026-02-03 23:24:30 -0300

    feat: adicionando loop para iterar sobre uma lista de varios servidores remotos

--- scripts/check-remote-servers.sh
+++ scripts/check-remote-servers.sh
@@ -13,3 +13,16 @@ if ping -c1 "$IP" &> /dev/null; then
 else
     echo "O servidor remoto nao esta disponivel"
 fi
+
+# Verificando a conexao com uma lista de servidores remotos
+HOSTS_FILE="/home/heitor/hosts.txt"
+
+for host in "$(cat $HOSTS_FILE)"
+
+do
+    if ping -c1 $host &> /dev/null; then
+        echo "O servidor $host esta disponivel"
+    else
+        echo "O servidor $host nao esta disponivel"
+    fi
+done
