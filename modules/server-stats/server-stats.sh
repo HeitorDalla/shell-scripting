b3cb02b1b3673ceddf9e68531df2ac5ab6a580aa
HeitorDalla <heitorvillavilla@gmail.com>
2026-02-16 08:12:42 -0300

    refactor: alterando estrutura de pastas para deixar o script junto de sua documentacao

--- modules/server-stats/server-stats.sh
+++ modules/server-stats/server-stats.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Script para monitoramento de CPU e processos
 # Alertar quando a CPU passar do limite e mostrar quem esta consumindo
@@ -7,12 +7,12 @@
 set -euo pipefail
 
 # Redirecionando todas as saídas para o arquivo log
-PATH_LOG="/mnt/c/Users/2992167/Desktop/monitor_cpu_log.log"
+LOG_FILE="$(dirname "$0")/server-stats.log"
 
-mkdir -p "$(dirname "$PATH_LOG")"
-touch "$PATH_LOG"
+mkdir -p "$(dirname "$LOG_FILE")"
+touch "$LOG_FILE"
 
-exec &> "$PATH_LOG"
+exec &> >(tee -a "$LOG_FILE")
 
 # Verificando se o usuario que esta executando o script é o root
 if [ "$EUID" -ne 0 ]; then
@@ -22,8 +22,15 @@ fi
 
 echo "--- Monitoramento de CPU e Processo ---"
 
-echo "--- Instalando dependências ---"
-pacman -Sy --noconfirm sysstat procps bc
+# Verificando dependencias
+DEPENDENCIAS=(awk top sleep date ps grep)
+
+for dep in "${DEPENDENCIAS[@]}"; do
+    if ! command -v "$dep" &> /dev/null;then
+        echo "Erro: a ferramenta $dep nao foi encontrada. Por favor, instale-a"
+        exit 1
+    fi
+done
 
 LIMITE_CPU="${LIMITE:-80}" # define um valor padrão que 'LIMITE' não existir ou está vazia
 CICLOS="${CICLOS:-3}"
@@ -32,8 +39,12 @@ INTERVALO="${INTERVALO:-60}"
 CONTADOR=0
 
 while true; do
+    echo "[$(date +%H:%M:%S)] Iniciando verificação de rotina..."
+
     # Ver uso total da CPU
     USO_CPU=$(LC_ALL=C mpstat 1 1 | awk '/Average:/ {printf("%.2f", 100 - $NF)}')
+
+    echo "Uso atual da CPU: $USO_CPU% (Limite: $LIMITE_CPU%)"
 
     # Verificando os 5 processos que mais utilizam CPU
     TOP5=$(top -b -n 1 | awk '{print $1, $2, $9, $10, $12}' | sed -n '7,12p')
@@ -52,6 +63,8 @@ while true; do
         echo "$TOP5"
         CONTADOR=0
     fi
+
+    echo "Aguardando ${INTERVALO}s para a próxima análise..."
 
     # Esperar o intervalo e repete
     sleep "$INTERVALO"
